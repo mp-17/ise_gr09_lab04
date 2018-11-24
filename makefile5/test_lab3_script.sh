7cea060f674ee4fb83a3e0e22504fdad85c2ba49
Ipponoo <pulettogiuseppe95@gmail.com>
2018-11-24 12:55:32 +0100

    corrected makefile 3 e makefile5 - added python command and -static option

--- makefile5/test_lab3_script.sh
+++ makefile5/test_lab3_script.sh
@@ -6,11 +6,13 @@ PATH_EXE='../ciao'
 MAIN_OUT_FILE='./outmain'
 KNOWN_OUT='./knowout'
 DIR_UNIVERSALDRAWER_PYTHON='./py_test_script/universalDrawer.py'
+DIR_COMMANDSGENERATOR_PYTHON='./py_test_script/inputGenerator.py'
 
 while read command
 do
     var1=$var1$command
 done < $DIR_COMMANDSFILE
+python $DIR_COMMANDSGENERATOR_PYTHON
 python $DIR_UNIVERSALDRAWER_PYTHON
 echo $var1 #| ./$PATH_EXE
 #diff -s $MAIN_OUT_FILE $KNOWN_OUT
