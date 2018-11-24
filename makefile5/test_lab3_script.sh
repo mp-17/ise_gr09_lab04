f5223f012bf481bb5c5e4079e52e5e5ff1056c88
Ipponoo <pulettogiuseppe95@gmail.com>
2018-11-24 12:37:26 +0100

    corrected makefile 5 - added python command

--- makefile5/test_lab3_script.sh
+++ makefile5/test_lab3_script.sh
@@ -5,12 +5,12 @@ DIR_COMMANDSFILE='./file_commands.txt'
 PATH_EXE='../ciao'
 MAIN_OUT_FILE='./outmain'
 KNOWN_OUT='./knowout'
-
+DIR_UNIVERSALDRAWER_PYTHON='./py_test_script/universalDrawer.py'
 
 while read command
 do
     var1=$var1$command
 done < $DIR_COMMANDSFILE
-
+python $DIR_UNIVERSALDRAWER_PYTHON
 echo $var1 #| ./$PATH_EXE
 #diff -s $MAIN_OUT_FILE $KNOWN_OUT
