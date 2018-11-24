e3a7029bf9be700cd61af67c220176323f5065ae
mp-17 <twisted@live.it>
2018-11-25 00:54:35 +0100

    fixed bug of pythonEmulator script: dx and dy for ellipses can't be 0

--- test/script/inputGenerator.py
+++ test/script/inputGenerator.py
@@ -24,8 +24,8 @@ while a < x :
         y2=random.randint(0,MAX_Y)
         cmd='L'+('{0:03d}'.format(x1))+('{0:03d}'.format(y1))+('{0:03d}'.format(x2))+('{0:03d}'.format(y2))+str(mode)+'\n'
     elif func == 2 :
-        xd=random.randint(0,MAX_XD)
-        yd=random.randint(0,MAX_YD)
+        xd=random.randint(1,MAX_XD)
+        yd=random.randint(1,MAX_YD)
         cmd='E'+('{0:03d}'.format(x1))+('{0:03d}'.format(y1))+('{0:03d}'.format(xd))+('{0:03d}'.format(yd))+str(mode)+'\n'
     f.write(cmd)    
     a+=1
