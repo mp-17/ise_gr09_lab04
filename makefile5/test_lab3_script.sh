4163e048d8a026504219497536f7fad5729f8978
Ipponoo <pulettogiuseppe95@gmail.com>
2018-11-24 13:00:03 +0100

    corrected makefile5 - order python commands

--- makefile5/test_lab3_script.sh
+++ makefile5/test_lab3_script.sh
@@ -8,11 +8,11 @@ KNOWN_OUT='./knowout'
 DIR_UNIVERSALDRAWER_PYTHON='./py_test_script/universalDrawer.py'
 DIR_COMMANDSGENERATOR_PYTHON='./py_test_script/inputGenerator.py'
 
+python $DIR_COMMANDSGENERATOR_PYTHON
 while read command
 do
     var1=$var1$command
 done < $DIR_COMMANDSFILE
-python $DIR_COMMANDSGENERATOR_PYTHON
 python $DIR_UNIVERSALDRAWER_PYTHON
 echo $var1 #| ./$PATH_EXE
 #diff -s $MAIN_OUT_FILE $KNOWN_OUT
